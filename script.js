66d0ac12c497d6ee6711983e661042fb4c8395ca
ANKIT KUMAR RAI <117643219+Ankit0315@users.noreply.github.com>
2023-04-13 13:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 //your JS code here. If required.
 var c = document.getElementbyId('level').value;
-alert('The level of the element is'+value);
+
+var level = 0;
+while (c.parentNode) {
+  level++;
+  c = c.parentNode;
+}
+alert("DOM level of #level:",level);
